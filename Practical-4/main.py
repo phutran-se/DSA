99bebb003a60738cb41eebdf409996a24adf7284
Phu Tran <21985654@student.curtin.edu.au>
2025-03-19 16:13:12 +0800

    Practical-4: refine main.py program

--- Practical-4/main.py
+++ Practical-4/main.py
@@ -24,7 +24,8 @@ def main():
 
     linked_list = DSALinkedList() # Create a new linked list
     Stop = False
-    while not Stop:
+    while not Stop: # Loop until the user chooses to exit
+        # Create menu options
         print("\nLinked List Menu:")
         print("1. Insert First")
         print("2. Insert Last")
@@ -36,41 +37,44 @@ def main():
         print()
         
         choice = input("Enter your choice [1-7]: ")
-        if choice == "1":
+        if choice == "1": # Insert First
             value = input("Enter value to insert at first: ")
             linked_list.insert_first(value)
-        elif choice == "2":
+        elif choice == "2": # Insert Last
             value = input("Enter value to insert at last: ")
             linked_list.insert_last(value)
             print_info(f"> Inserted: {value}")
-        elif choice == "3":
+        elif choice == "3": # Remove First
+            # Try to remove the first value and catch any exceptions
             try:
                 rm = linked_list.remove_first()
                 print_info(f"> Removed first: {rm}")
             except Exception as e:
                 print_error(f"> ERROR on removing first: {e}")
-        elif choice == "4":
+        elif choice == "4": # Remove Last
+            # Try to remove the last value and catch any exceptions
             try:
                 rm = linked_list.remove_last()
                 print_info(f"> Removed last: {rm}")
             except Exception as e:
                 print_error(f"> ERROR on removing last: {e}")
-        elif choice == "5":
+        elif choice == "5": # Display the content of linked list 
             print_info(f"> List = {linked_list.display()}")
-        elif choice == "6":
-            value = input("Enter value to find: ")
-            found = linked_list.find_first(value)
-            if found != -1:
-                print_info2(f"> Value '{value}' found at index: {found}")
-            else:
-                print_error(f"> Value '{value}' not found in list")
-        elif choice == "7":
-            Stop = True
+        elif choice == "6": # Find the first occurrence of a value
+            val = input("Enter value to find: ")
+            found = linked_list.find_first(val)
+            if found != -1: # If the value is found
+                print_info2(f"> Value '{val}' found at index: {found}")
+            else: # If the value is not found
+                print_error(f"> Value '{val}' not found in list")
+        elif choice == "7": # Exit
+            Stop = True # Set the stop flag to True
             print_error("Exiting...")
         else:
-            print_error("Invalid choice. Please try again.")
+            print_error("Your choice is invalid. Please try again!!!")
 
 if __name__ == "__main__":
+    # Run the main function and try to catch any exceptions to avoid the program from crashing
     try: 
         main()
     except Exception as e:
