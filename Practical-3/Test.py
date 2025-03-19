d4272f3f4809298134a285798eab1c430bb14d3f
Phu Tran <21985654@student.curtin.edu.au>
2025-03-19 16:07:58 +0800

    Practical-3: refine Test.py with color for printed text

--- Practical-3/Test.py
+++ Practical-3/Test.py
@@ -7,82 +7,88 @@ Student 2: Minh Quoc Chau Cao (21803620)
 
 from DSAStack import DSAStack
 from DSAQueue import ShufflingQueue, CircularQueue
-
+from Funct import print_info, print_error
 
 # Test DSAStack
 def test_dsa_stack():
-    print("Testing DSAStack...")
+    print("Testing class DSAStack...")
     stack = DSAStack(5)
     
     # Test is_empty
-    assert stack.is_empty() == True, "Test is_empty failed"
+    assert stack.is_empty() == True, "Test is_empty() failed"
     
     # Test push
     stack.push(1)
     stack.push(2)
     stack.push(3)
-    assert stack.stack == [1, 2, 3, None, None], "Test push failed"
+    assert stack.stack == [1, 2, 3, None, None], "Test push() failed"
     
     # Test is_full
-    assert stack.is_full() == False, "Test is_full failed"
+    assert stack.is_full() == False, "Test is_full() failed"
     
     # Test pop
-    assert stack.pop() == 3, "Test pop failed"
-    assert stack.stack == [1, 2, None, None, None], "Test pop failed"
+    assert stack.pop() == 3, "Test pop() failed"
+    assert stack.stack == [1, 2, None, None, None], "Test pop() failed"
     
     # Test peek
-    assert stack.peek() == 2, "Test peek failed"
-    print("All tests passed for DSAStack!\n")
+    assert stack.peek() == 2, "Test peek() failed"
+
+    # Finally, print that all tests passed
+    print_info("All tests passed for DSAStack!\n")
 
 # Test ShufflingQueue
 def test_shuffling_queue():
-    print("Testing ShufflingQueue...")
+    print("Testing class ShufflingQueue...")
     queue = ShufflingQueue(5)
     
     # Test is_empty
-    assert queue.is_empty() == True, "Test is_empty failed"
+    assert queue.is_empty() == True, "Test is_empty() failed"
     
     # Test enqueue
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(3)
-    assert queue.queue == [1, 2, 3, None, None], "Test enqueue failed"
+    assert queue.queue == [1, 2, 3, None, None], "Test enqueue() failed"
     
     # Test is_full
-    assert queue.is_full() == False, "Test is_full failed"
+    assert queue.is_full() == False, "Test is_full() failed"
     
     # Test dequeue
-    assert queue.dequeue() == 1, "Test dequeue failed"
-    assert queue.queue == [2, 3, None, None, None], "Test dequeue failed"
+    assert queue.dequeue() == 1, "Test dequeue() failed"
+    assert queue.queue == [2, 3, None, None, None], "Test dequeue() failed"
     
     # Test peek
-    assert queue.peek() == 2, "Test peek failed"
-    print("All tests passed for ShufflingQueue!\n")
+    assert queue.peek() == 2, "Test peek() failed"
+
+    # Finally, print that all tests passed
+    print_info("All tests passed for ShufflingQueue!\n")
 
 # Test CircularQueue
 def test_circular_queue():
-    print("Testing CircularQueue...")
+    print("Testing class CircularQueue...")
     queue = CircularQueue(5)
     
     # Test is_empty
-    assert queue.is_empty() == True, "Test is_empty failed"
+    assert queue.is_empty() == True, "Test is_empty() failed"
     
     # Test enqueue
     queue.enqueue(1)
     queue.enqueue(2)
     queue.enqueue(3)
-    assert queue.queue == [1, 2, 3, None, None], "Test enqueue failed"
+    assert queue.queue == [1, 2, 3, None, None], "Test enqueue() failed"
     
     # Test is_full
-    assert queue.is_full() == False, "Test is_full failed"
+    assert queue.is_full() == False, "Test is_full() failed"
     
     # Test dequeue
-    assert queue.dequeue() == 1, "Test dequeue failed"
-    assert queue.queue == [None, 2, 3, None, None], "Test dequeue failed"
+    assert queue.dequeue() == 1, "Test dequeue() failed"
+    assert queue.queue == [None, 2, 3, None, None], "Test dequeue() failed"
     
     # Test peek
-    assert queue.peek() == 2, "Test peek failed"
-    print("All tests passed for CircularQueue!\n")
+    assert queue.peek() == 2, "Test peek() failed"
+
+    # Finally, print that all tests passed
+    print_info("All tests passed for CircularQueue!\n")
 
 # Run the tests
 test_dsa_stack()
